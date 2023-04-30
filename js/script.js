4709b9d48bbb8ddc4a9c59462f94bc721c1d7d58
Pablo Chaparro <pablo.47.114@gmail.com>
2023-04-30 02:12:51 -0300

    youtube navbar

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const upload = document.querySelector(".upload")
+const notifications = document.querySelector(".notifications")
+const uploadContainer = document.querySelector(".uploadWindow")
+const uploadIcon = document.querySelector(".bi-camera-video")
+const uploadIconFill = document.querySelector(".bi-camera-video-fill")
+const notificationsContainer = document.querySelector(".notificationsWindow")
+const notificationsIcon = document.querySelector(".bi-bell")
+const notificationsIconFill = document.querySelector(".bi-bell-fill")
+const cuenta = document.querySelector(".cuenta")
+const cuentaContainer = document.querySelector(".cuentaWindow")
+
+const accountMenusFadeInOut = (div,div2,icon1,icon2)=>{
+    div.addEventListener("focusin", event =>{
+        event = div2
+        event.style.display="flex";
+        icon1.style.display="none";
+        icon2.style.display="flex";
+    })
+
+    div.addEventListener("focusout", event =>{
+        event = div2
+        event.style.display="none";
+        icon1.style.display="flex";
+        icon2.style.display="none";
+    })
+}
+
+const cuentaMenuFadeInOut = (div,divcontainer) =>{
+    div.addEventListener("focusin", event =>{
+        event = divcontainer
+        event.style.display="flex";
+    })
+    div.addEventListener("focusout", event =>{
+        event = divcontainer
+        event.style.display="none";
+    })
+}
+accountMenusFadeInOut(upload,uploadContainer,uploadIcon,uploadIconFill)
+accountMenusFadeInOut(notifications,notificationsContainer,notificationsIcon,notificationsIconFill)
+cuentaMenuFadeInOut(cuenta,cuentaContainer)
